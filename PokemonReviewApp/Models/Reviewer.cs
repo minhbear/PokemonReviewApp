61bff73c24bf1ffb258501599275888f7da26a4e
MINHNHATNGUYEN\Minh Nguyen <minhbeardev@gmail.com>
2023-09-10 23:43:35 +0700

    feat: add model

---
+++ PokemonReviewApp/Models/Reviewer.cs
@@ -0,0 +1,9 @@
+﻿namespace PokemonReviewApp.Models
+{
+    public class Reviewer
+    {
+        public int Id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+    }
+}
